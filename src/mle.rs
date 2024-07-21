6183bd9865ac15483974399986928e2e907f76a7
Remco Bloemen <remco@wicked.ventures>
2024-07-21 11:32:31 +0200

    product check

--- src/mle.rs
+++ src/mle.rs
@@ -1,7 +1,7 @@
 use {
     crate::{transcript, ProverTranscript, VerifierTranscript},
     ark_bn254::Fr,
-    ark_ff::One,
+    ark_ff::{One, Zero},
     rayon,
 };
 
@@ -31,24 +31,48 @@ pub fn eval_mle(coefficients: &[Fr], eval: &[Fr]) -> Fr {
     }
 }
 
-// TODO: This is destructive on coefficients, but only overwrites first half.
-// We can restore the original requires n/2 space.
-pub fn prove_sumcheck(transcript: &mut ProverTranscript, mut coefficients: &mut [Fr], size: usize) {
-    assert_eq!(coefficients.len(), 1 << size);
-    for _ in 0..size {
-        let (c0, c1) = coefficients.split_at_mut(coefficients.len() / 2);
-        transcript.write(c0.iter().sum());
-        transcript.write(c1.iter().sum());
-        let r = transcript.read();
-        // TODO: Single pass update and compute next sums.
-        // eq(r, 0) * c0 + eq(r, 1) * c1 = c0 + r * (c1 - c0)
-        c0.iter_mut()
-            .zip(c1.iter())
-            .for_each(|(c0, c1)| *c0 += r * (*c1 - *c0));
-        coefficients = c0;
-    }
+/// Updates f(x, x') -> f(r, x') and returns f
+fn update(f: &mut [Fr], r: Fr) -> &mut [Fr] {
+    let (a, b) = f.split_at_mut(f.len() / 2);
+    a.iter_mut().zip(b).for_each(|(a, b)| *a += r * (*b - *a));
+    a
 }
 
+/// Prove sumcheck for $\sum_x f(x)$.
+/// Returns $(e, r)$ for reduced claim $e = f(r)$.
+// TODO: This is destructive on coefficients, but only overwrites first half.
+// We can restore the original requires n/2 space.
+pub fn prove_sumcheck(
+    transcript: &mut ProverTranscript,
+    size: usize,
+    mut f: &mut [Fr],
+    mut sum: Fr,
+) -> (Fr, Vec<Fr>) {
+    assert_eq!(f.len(), 1 << size);
+    let mut rs = Vec::with_capacity(size);
+    for _ in 0..size {
+        // Compute $p(x) = \sum_y f(x, y) = eq(x, 0) \sum_y f(0, y) + eq(x, 1) \sum_y f(1, y)$
+        let f0y = f.iter().take(f.len() / 2).sum();
+        let f1y = f.iter().skip(f.len() / 2).sum();
+        assert_eq!(f0y + f1y, sum); // TODO: Exploit redudancy to avoid computations.
+
+        // Send $p(0)$ and $p(1)$ to verifier.
+        // Note: This is not ZK.
+        transcript.write(f0y);
+        transcript.write(f1y);
+        let r = transcript.read();
+        rs.push(r);
+
+        // TODO: Fold update with sum computation.
+        f = update(f, r);
+        // sum = p(r) = eq(r, 0) \sum_y f(0, y) + eq(r, 1) \sum_y f(1, y)
+        sum = f0y + r * (f1y - f0y);
+    }
+    (f[0], rs)
+}
+
+/// Verify sumcheck for $e = \sum_x f(x)$.
+/// Returns $(e, r)$ for reduced claim $e = f(r)$.
 pub fn verify_sumcheck(
     transcript: &mut VerifierTranscript,
     size: usize,
@@ -66,6 +90,75 @@ pub fn verify_sumcheck(
         e = r * (e1 - e0) + e0;
     }
     (e, rs)
+}
+
+/// Prove sumcheck for $\sum_x f(x) ⋅ g(x)$.
+/// Returns $(e, r)$ for reduced claim $e = f(r)⋅ g(r)$.
+pub fn prove_sumcheck_product(
+    transcript: &mut ProverTranscript,
+    size: usize,
+    mut f: &mut [Fr],
+    mut g: &mut [Fr],
+    mut sum: Fr,
+) -> (Fr, Vec<Fr>) {
+    assert_eq!(f.len(), 1 << size);
+    assert_eq!(g.len(), 1 << size);
+    let mut rs = Vec::with_capacity(size);
+    for _ in 0..size {
+        let (f0, f1) = f.split_at(f.len() / 2);
+        let (g0, g1) = g.split_at(g.len() / 2);
+        let p00: Fr = f0.iter().zip(g0.iter()).map(|(f, g)| f * g).sum();
+        let p01: Fr = f0.iter().zip(g1.iter()).map(|(f, g)| f * g).sum();
+        let p10: Fr = f1.iter().zip(g0.iter()).map(|(f, g)| f * g).sum();
+        let p11: Fr = f1.iter().zip(g1.iter()).map(|(f, g)| f * g).sum();
+        // p(x) = p0 + p1 ⋅ x + p2 ⋅ x^2
+        let p0 = p00;
+        let p1 = p01 + p10 - p00 - p00;
+        let p2 = p00 + p11 - p01 - p10;
+        // p(0) + p(1) = 2 ⋅ p0 + p1 + p2
+        assert_eq!(p0 + p0 + p1 + p2, sum);
+        transcript.write(p0);
+        transcript.write(p1);
+        transcript.write(p2);
+        let r = transcript.read();
+        rs.push(r);
+        f = update(f, r);
+        g = update(g, r);
+        sum = p0 + r * (p1 + r * p2);
+    }
+    (sum, rs)
+}
+
+pub fn verify_sumcheck_quadratic(
+    transcript: &mut VerifierTranscript,
+    size: usize,
+    mut e: Fr,
+) -> (Fr, Vec<Fr>) {
+    let mut rs = Vec::with_capacity(size);
+    for i in 0..size {
+        let p0 = transcript.read();
+        let p1 = transcript.read();
+        let p2 = transcript.read();
+        if e != p0 + p0 + p1 + p2 {
+            panic!("Sumcheck failed at step {i}");
+        }
+        let r = transcript.generate();
+        rs.push(r);
+        e = p0 + r * (p1 + r * p2);
+    }
+    (e, rs)
+}
+
+/// Sumcheck for $\sum_x e(x) ⋅ (a(x) ⋅ b(x) - c(x))$.
+/// Returns $(e, r)$ for reduced claim $e = e(r) ⋅ (a(r) ⋅ b(r) - c(r))$.
+pub fn prove_sumcheck_r1cs(
+    transcript: &mut ProverTranscript,
+    size: usize,
+    mut e: &mut [Fr],
+    mut a: &mut [Fr],
+    mut b: &mut [Fr],
+    mut c: &mut [Fr],
+) {
 }
 
 #[cfg(test)]
@@ -101,18 +194,49 @@ mod test {
     fn test_sumcheck() {
         let size = 10;
         let mut rng = ChaCha20Rng::from_entropy();
-        let mut f = (0..1 << size).map(|_| rng.gen()).collect::<Vec<Fr>>();
+        let f = (0..1 << size).map(|_| rng.gen()).collect::<Vec<Fr>>();
+        let s = f.iter().sum();
 
         // Prove
         let mut transcript = ProverTranscript::new();
-        transcript.write(f.iter().sum());
-        prove_sumcheck(&mut transcript, &mut f, size);
+        transcript.write(s);
+        let mut copy = f.clone();
+        let (e, rs) = prove_sumcheck(&mut transcript, size, &mut copy, s);
+        assert_eq!(eval_mle(&f, &rs), e);
         let proof = transcript.finish();
         dbg!(proof.len() * std::mem::size_of::<Fr>());
 
         // Verify
         let mut transcript = VerifierTranscript::new(&proof);
         let e = transcript.read();
-        let _ = verify_sumcheck(&mut transcript, size, e);
+        let (e, rs) = verify_sumcheck(&mut transcript, size, e);
+        assert_eq!(eval_mle(&f, &rs), e);
+    }
+
+    #[test]
+    fn test_sumcheck_product() {
+        let size = 10;
+        let mut rng = ChaCha20Rng::from_entropy();
+        let f = (0..1 << size).map(|_| rng.gen()).collect::<Vec<Fr>>();
+        let g = (0..1 << size).map(|_| rng.gen()).collect::<Vec<Fr>>();
+        let s = f.iter().zip(g.iter()).map(|(f, g)| f * g).sum();
+
+        // Prove
+        let mut transcript = ProverTranscript::new();
+        transcript.write(s);
+        let mut fc = f.clone();
+        let mut gc = g.clone();
+        let (e, rs) = prove_sumcheck_product(&mut transcript, size, &mut fc, &mut gc, s);
+        assert_eq!(eval_mle(&f, &rs) * eval_mle(&g, &rs), e);
+        let proof = transcript.finish();
+        dbg!(proof.len() * std::mem::size_of::<Fr>());
+
+        // Verify
+        let mut transcript = VerifierTranscript::new(&proof);
+        let vs = transcript.read();
+        assert_eq!(vs, s);
+        let (ve, vrs) = verify_sumcheck_quadratic(&mut transcript, size, s);
+        assert_eq!(ve, e);
+        assert_eq!(vrs, rs);
     }
 }
