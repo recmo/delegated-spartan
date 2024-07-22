5499d5331c728757050031a645aa9f7b7a845946
Remco Bloemen <remco@wicked.ventures>
2024-07-22 17:16:32 +0200

    Generic verify

--- src/mle.rs
+++ src/mle.rs
@@ -1,9 +1,14 @@
 use {
     crate::{transcript, ProverTranscript, VerifierTranscript},
     ark_bn254::Fr,
-    ark_ff::{One, Zero},
+    ark_ff::{Field, MontFp, One, Zero},
+    itertools::izip,
     rayon,
+    std::array,
 };
+
+const HALF: Fr =
+    MontFp!("10944121435919637611123202872628637544274182200208017171849102093287904247809");
 
 /// Evaluates a multilinear extension at a point (parallel version).
 pub fn par_eval_mle(coefficients: &[Fr], eval: &[Fr]) -> Fr {
@@ -52,44 +57,20 @@ pub fn prove_sumcheck(
     let mut rs = Vec::with_capacity(size);
     for _ in 0..size {
         // Compute $p(x) = \sum_y f(x, y) = eq(x, 0) \sum_y f(0, y) + eq(x, 1) \sum_y f(1, y)$
-        let f0y = f.iter().take(f.len() / 2).sum();
-        let f1y = f.iter().skip(f.len() / 2).sum();
-        assert_eq!(f0y + f1y, sum); // TODO: Exploit redudancy to avoid computations.
-
-        // Send $p(0)$ and $p(1)$ to verifier.
-        // Note: This is not ZK.
-        transcript.write(f0y);
-        transcript.write(f1y);
+        // Send p(x) = p0 + p1 ⋅ x to verifier
+        let p0 = f.iter().take(f.len() / 2).sum();
+        let p1 = sum - p0 - p0;
+        transcript.write(p0);
+        transcript.write(p1);
         let r = transcript.read();
         rs.push(r);
 
         // TODO: Fold update with sum computation.
         f = update(f, r);
         // sum = p(r) = eq(r, 0) \sum_y f(0, y) + eq(r, 1) \sum_y f(1, y)
-        sum = f0y + r * (f1y - f0y);
+        sum = p0 + r * p1;
     }
     (f[0], rs)
-}
-
-/// Verify sumcheck for $e = \sum_x f(x)$.
-/// Returns $(e, r)$ for reduced claim $e = f(r)$.
-pub fn verify_sumcheck(
-    transcript: &mut VerifierTranscript,
-    size: usize,
-    mut e: Fr,
-) -> (Fr, Vec<Fr>) {
-    let mut rs = Vec::with_capacity(size);
-    for _ in 0..size {
-        let e0 = transcript.read();
-        let e1 = transcript.read();
-        if e != e0 + e1 {
-            panic!("Sumcheck failed");
-        }
-        let r = transcript.generate();
-        rs.push(r);
-        e = r * (e1 - e0) + e0;
-    }
-    (e, rs)
 }
 
 /// Prove sumcheck for $\sum_x f(x) ⋅ g(x)$.
@@ -105,18 +86,22 @@ pub fn prove_sumcheck_product(
     assert_eq!(g.len(), 1 << size);
     let mut rs = Vec::with_capacity(size);
     for _ in 0..size {
+        // p(x) = p0 + p1 ⋅ x + p2 ⋅ x^2
+        let mut p0 = Fr::zero();
+        let mut p2 = Fr::zero();
         let (f0, f1) = f.split_at(f.len() / 2);
         let (g0, g1) = g.split_at(g.len() / 2);
-        let p00: Fr = f0.iter().zip(g0.iter()).map(|(f, g)| f * g).sum();
-        let p01: Fr = f0.iter().zip(g1.iter()).map(|(f, g)| f * g).sum();
-        let p10: Fr = f1.iter().zip(g0.iter()).map(|(f, g)| f * g).sum();
-        let p11: Fr = f1.iter().zip(g1.iter()).map(|(f, g)| f * g).sum();
-        // p(x) = p0 + p1 ⋅ x + p2 ⋅ x^2
-        let p0 = p00;
-        let p1 = p01 + p10 - p00 - p00;
-        let p2 = p00 + p11 - p01 - p10;
-        // p(0) + p(1) = 2 ⋅ p0 + p1 + p2
-        assert_eq!(p0 + p0 + p1 + p2, sum);
+        f0.iter()
+            .zip(f1)
+            .zip(g0.iter().zip(g1))
+            .for_each(|((f0, f1), (g0, g1))| {
+                // Evaluation at 0
+                p0 += f0 * g0;
+                // Evaluation at ∞
+                p2 += (f1 - f0) * (g1 - g0);
+            });
+        // sum = p(0) + p(1) = 2 ⋅ p0 + p1 + p2
+        let p1 = sum - p0 - p0 - p2;
         transcript.write(p0);
         transcript.write(p1);
         transcript.write(p2);
@@ -124,29 +109,10 @@ pub fn prove_sumcheck_product(
         rs.push(r);
         f = update(f, r);
         g = update(g, r);
+        // sum = p(r)
         sum = p0 + r * (p1 + r * p2);
     }
     (sum, rs)
-}
-
-pub fn verify_sumcheck_quadratic(
-    transcript: &mut VerifierTranscript,
-    size: usize,
-    mut e: Fr,
-) -> (Fr, Vec<Fr>) {
-    let mut rs = Vec::with_capacity(size);
-    for i in 0..size {
-        let p0 = transcript.read();
-        let p1 = transcript.read();
-        let p2 = transcript.read();
-        if e != p0 + p0 + p1 + p2 {
-            panic!("Sumcheck failed at step {i}");
-        }
-        let r = transcript.generate();
-        rs.push(r);
-        e = p0 + r * (p1 + r * p2);
-    }
-    (e, rs)
 }
 
 /// Sumcheck for $\sum_x e(x) ⋅ (a(x) ⋅ b(x) - c(x))$.
@@ -158,7 +124,78 @@ pub fn prove_sumcheck_r1cs(
     mut a: &mut [Fr],
     mut b: &mut [Fr],
     mut c: &mut [Fr],
-) {
+    mut sum: Fr,
+) -> (Fr, Vec<Fr>) {
+    assert_eq!(e.len(), 1 << size);
+    assert_eq!(a.len(), 1 << size);
+    assert_eq!(b.len(), 1 << size);
+    assert_eq!(c.len(), 1 << size);
+    let mut rs = Vec::with_capacity(size);
+    for _ in 0..size {
+        // p(x) = p0 + p1 ⋅ x + p2 ⋅ x^2 + p3 ⋅ x^3
+        let mut p0 = Fr::zero();
+        let mut p2 = Fr::zero();
+        let mut p3 = Fr::zero();
+        let (e0, e1) = e.split_at(e.len() / 2);
+        let (a0, a1) = a.split_at(a.len() / 2);
+        let (b0, b1) = b.split_at(b.len() / 2);
+        let (c0, c1) = c.split_at(c.len() / 2);
+        izip!(
+            e0.iter().zip(e1),
+            a0.iter().zip(a1),
+            b0.iter().zip(b1),
+            c0.iter().zip(c1)
+        )
+        .for_each(|(e, a, b, c)| {
+            // Evaluation at 0
+            p0 += *e.0 * (a.0 * b.0 - c.0);
+            // Evaluation at -1
+            p2 += (e.0 + e.0 - e.1) * ((a.0 + a.0 - a.1) * (b.0 + b.0 - b.1) - (c.0 + c.0 - c.1));
+            // Evaluation at ∞
+            p3 += (e.1 - e.0) * (a.1 - a.0) * (b.1 - b.0);
+        });
+        // p(-1)             =     p0 - p1 + p2 - p3
+        // sum = p(0) + p(1) = 2 ⋅ p0 + p1 + p2 + p3
+        let p1 = sum - p0 - p0 - p2 - p3;
+        transcript.write(p0);
+        transcript.write(p1);
+        transcript.write(p2);
+        let r = transcript.read();
+        rs.push(r);
+        e = update(e, r);
+        a = update(a, r);
+        b = update(b, r);
+        c = update(c, r);
+        // sum = p(r)
+        sum = p0 + r * (p1 + r * p2);
+    }
+    (sum, rs)
+}
+
+/// Verify sumcheck for $N$-term polynomials.
+/// I.e. N = 2 for linear, 3 for quadratic, etc.
+pub fn verify_sumcheck<const N: usize>(
+    transcript: &mut VerifierTranscript,
+    size: usize,
+    mut e: Fr,
+) -> (Fr, Vec<Fr>) {
+    let mut rs = Vec::with_capacity(size);
+    for i in 0..size {
+        let p: [Fr; N] = array::from_fn(|_| transcript.read());
+        // Check p'(r) = p(0) + p(1)
+        if e != p[0] + p.iter().sum::<Fr>() {
+            panic!("Sumcheck failed at step {i}");
+        }
+        assert_eq!(p[0], e - p.iter().sum::<Fr>());
+        let r = transcript.generate();
+        rs.push(r);
+        e = p
+            .into_iter()
+            .rev()
+            .reduce(|acc, p| p + r * acc)
+            .expect("p not empty");
+    }
+    (e, rs)
 }
 
 #[cfg(test)]
@@ -168,6 +205,11 @@ mod test {
         rand::{Rng, SeedableRng},
         rand_chacha::ChaCha20Rng,
     };
+
+    #[test]
+    fn test_half() {
+        assert_eq!(HALF.double(), Fr::ONE);
+    }
 
     #[test]
     fn test_eval_mle_1() {
@@ -209,7 +251,7 @@ mod test {
         // Verify
         let mut transcript = VerifierTranscript::new(&proof);
         let e = transcript.read();
-        let (e, rs) = verify_sumcheck(&mut transcript, size, e);
+        let (e, rs) = verify_sumcheck::<2>(&mut transcript, size, e);
         assert_eq!(eval_mle(&f, &rs), e);
     }
 
@@ -235,7 +277,7 @@ mod test {
         let mut transcript = VerifierTranscript::new(&proof);
         let vs = transcript.read();
         assert_eq!(vs, s);
-        let (ve, vrs) = verify_sumcheck_quadratic(&mut transcript, size, s);
+        let (ve, vrs) = verify_sumcheck::<3>(&mut transcript, size, s);
         assert_eq!(ve, e);
         assert_eq!(vrs, rs);
     }
